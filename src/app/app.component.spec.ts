bb87e220e411adc6d0676e372f8ff264c0ffdd98
vitorvenacio <vitorvenacio@gmail.com>
2021-08-04 15:02:46 -0300

    Fim do projeto gerenciador de tarefa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
       ],
       declarations: [
         AppComponent
@@ -20,16 +23,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gerenciadortarefas'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gerenciadortarefas');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciadortarefas app is running!');
-  });
+  
 });
--- src/app/tarefas/index.ts
+++ src/app/tarefas/index.ts
@@ -1,2 +1,6 @@
 export * from './tarefas.module';
-
+export * from './shared';
+export * from './listar';
+export * from './tarefas-routing.module';
+export * from './cadastrar';
+export * from './editar';
